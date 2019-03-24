5ea6d5a57ee744fd515112302cbd4ff3fb02faf3
Adam Magaluk <AdamMagaluk@google.com>
2019-03-24 13:26:14 -0400

    Sample InfluxDB client sample. Will be removed soon.

--- src/test/java/co/cask/influxdb/InfluxDBTest.java
+++ src/test/java/co/cask/influxdb/InfluxDBTest.java
@@ -1,5 +1,35 @@
 package co.cask.influxdb;
 
+import java.util.concurrent.TimeUnit;
+import org.influxdb.BatchOptions;
+import org.influxdb.InfluxDB;
+import org.influxdb.InfluxDBFactory;
+import org.influxdb.dto.Point;
+import org.influxdb.dto.Query;
+import org.junit.Test;
+
 public class InfluxDBTest {
 
+  @Test
+  public void testBasic() throws Throwable {
+    InfluxDB influxDB = InfluxDBFactory.connect("http://localhost:8086", "root", "root");
+
+    String dbName = "aTimeSeries";
+    influxDB.query(new Query("CREATE DATABASE " + dbName));
+    influxDB.setDatabase(dbName);
+
+    influxDB.enableBatch(BatchOptions.DEFAULTS);
+
+    Point point =
+        Point.measurement("cpu")
+            .time(System.currentTimeMillis(), TimeUnit.MILLISECONDS)
+            .addField("idle", 90L)
+            .addField("user", 9L)
+            .addField("system", 2L)
+            .build();
+
+    influxDB.write(point);
+
+    influxDB.close();
+  }
 }
